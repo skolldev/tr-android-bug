d0c4560d799a04d416e983f880f88e8a2ea24f0a
Alex May <alex.may@studysmarter.de>
2022-02-09 12:31:00 +0100

    add minimum reproduction for bug

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  ImageOptions,
+} from '@capacitor/camera';
+import { defer } from 'rxjs';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
   constructor() {}
 
+  public takePicture() {
+    const options: ImageOptions = {
+      resultType: CameraResultType.Uri,
+      quality: 50,
+      source: CameraSource.Camera,
+      correctOrientation: true,
+    };
+
+    defer(() => Camera.getPhoto(options)).subscribe((photo) =>
+      console.log(photo)
+    );
+  }
 }
